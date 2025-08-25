0e4bdfb3e4f5f533dc172470579a47688985497a
Damola <adebitandamola02@gmail.com>
2025-08-25 22:00:08 +0100

    First Dojo commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const NavBar = () => {
+  const handleClick = (name) => {
+    console.log(`Hello ${name}`);
+  };
+  return (
+    <nav className="navbar">
+      <h1>Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+        <button onClick={() => handleClick("Jay")}>click</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
