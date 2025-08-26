7fd5ba7b1edef7098428b1ef8b5fc260a305b568
Damola <adebitandamola02@gmail.com>
2025-08-26 23:56:37 +0100

    changed name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,14 @@ const NavBar = () => {
   const handleClick = (name) => {
     console.log(`Hello ${name}`);
   };
+
   return (
     <nav className="navbar">
       <h1>Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
-        <button onClick={() => handleClick("Jay")}>click</button>
+        <button onClick={() => handleClick("day")}>click</button>
       </div>
     </nav>
   );
